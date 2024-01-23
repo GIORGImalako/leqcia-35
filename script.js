c4e2c2c46f4fef82d10459589940b137603e4a1f
Giorgi Malakmadze <malakmadzegio5@gmail.com>
2024-01-23 19:41:55 +0400

    new functions

---
+++ script.js
@@ -0,0 +1,40 @@
+//function test() {
+//   console.log(1)
+//   console.log(2)
+//   console.log(3)
+//}
+//for( let i = 0; i <= 5; i++) {
+//   test();
+//}
+
+//function welcome (saxeli, asaki) {
+//alert('welcome ${saxeli}, you are ${asaki} years old')
+//}
+//let saxeli = prompt ("enter your name")
+//let asaki = prompt ("enter your age")
+//welcome(saxeli, asaki)
+
+//function mimateba(a, b) {
+// let plus = a + b;
+//return plus
+
+//}
+//console.log(mimateba(25, 33));
+
+//let globaluri = "me var globaluri cvladi"
+
+//function testFunction() {
+//    console.log(globaluri)
+//}
+
+//function gamravleba (a,b, c) {
+// return a * b * c
+//}
+//let pasuxi = gamravleba(4,1,2,)
+//console.log(pasuxi)
+
+// arrow fuctions
+// let gamravleba=(x,y) => x * y;
+// let pasuxi = gamravleba (3,4);
+
+// console.log()
